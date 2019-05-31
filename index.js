5b19ea6e03fb8145feaa450f8ccd8c6fa7a547ac
anthony <stackchain@gmail.com>
2019-05-31 11:41:17 -0400

    commit - server and index done & working

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+
+
+
+const server = require('./server.js');
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+    console.log(`\n*** Server running on http//:localhost:${port}  ***\n`);
+});
+
+
+
+
+
+
+
 /*
 play this: https://www.youtube.com/watch?v=d-diB65scQU
 
