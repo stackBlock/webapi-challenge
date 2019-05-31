e9ba957b0e5b84c43f4f6f69a0f2a54f2e427f65
anthony <stackchain@gmail.com>
2019-05-31 13:47:46 -0400

    commit - mvp error message done - mvp done!

--- routes/api/actions.js
+++ routes/api/actions.js
@@ -6,20 +6,36 @@ const db = require('../../data/helpers/actionModel.js');
 router.get('/', async (req, res) => {
     await db.get().then(action => {
         res.json({ action })
-    });
+    })
+        .catch(error => {
+            res.status(500).json({ ErrorMessage: "Something bad has happened" });
+            return;
+        });
 });
 
 router.get('/:id', async (req, res) => {
     await db.get(req.params.id).then(function (data) {
         res.send(data);
-    });
+    })
+        .catch(error => {
+            res.status(500).json({ ErrorMessage: "Something bad has happened" });
+            return;
+        });
 });
 
 router.post('/', (req, res) => {
     const { project_id, description, notes, completed } = req.body;
     db.insert({ project_id, description, notes, completed }).then(response => {
-        res.status(201).json(response);
-    });
+        res.status(201).json(response)
+    })
+        .catch(error => {
+            if (error.errno === 19) {
+                res.status(400).json({ Error: `There is no project with that ID number` });
+                return;
+            }
+            res.status(400).json({ ErrorMessage: error });
+            return;
+        });
 });
 
 router.put('/:id', (req, res) => {
@@ -27,14 +43,22 @@ router.put('/:id', (req, res) => {
     const { project_id, description, notes } = req.body;
     db.update(id, { project_id, description, notes }).then(response => {
         res.status(201).json(response)
-    });
+    })
+        .catch(error => {
+            res.status(500).json({ ErrorMessage: "Something bad has happened" });
+            return;
+        });
 });
 
 router.delete('/:id', (req, res) => {
     const id = req.params.id;
     db.remove(id).then(response => {
         res.status(200).json({ Success: `User ${id} is gone forever from the system!!` })
-    });
+    })
+        .catch(error => {
+            res.status(500).json({ ErrorMessage: "Something bad has happened" });
+            return;
+        });
 });
 
 
--- routes/api/projects.js
+++ routes/api/projects.js
@@ -6,20 +6,32 @@ const db = require('../../data/helpers/projectModel.js');
 router.get('/', async (req, res) => {
     await db.get().then(projects => {
         res.json({ projects })
-    });
+    })
+        .catch(error => {
+            res.status(500).json({ ErrorMessage: "Something bad has happened" });
+            return;
+        });
 });
 
 router.get('/:id', async (req, res) => {
     await db.get(req.params.id).then(function (data) {
         res.send(data);
-    });
+    })
+        .catch(error => {
+            res.status(500).json({ ErrorMessage: "Something bad has happened" });
+            return;
+        });
 });
 
 router.post('/', (req, res) => {
     const { name, description, completed } = req.body;
     db.insert({ name, description, completed }).then(response => {
         res.status(201).json(response);
-    });
+    })
+        .catch(error => {
+            res.status(500).json({ ErrorMessage: "Something bad has happened" });
+            return;
+        });
 });
 
 router.put('/:id', (req, res) => {
@@ -27,14 +39,22 @@ router.put('/:id', (req, res) => {
     const { name, description, completed } = req.body;
     db.update(id, { name, description, completed }).then(response => {
         res.status(201).json(response)
-    });
+    })
+        .catch(error => {
+            res.status(500).json({ ErrorMessage: "Something bad has happened" });
+            return;
+        });
 });
 
 router.delete('/:id', (req, res) => {
     const id = req.params.id;
     db.remove(id).then(response => {
         res.status(200).json({ Success: `User ${id} is gone forever from the system!!` })
-    });
+    })
+        .catch(error => {
+            res.status(500).json({ ErrorMessage: "Something bad has happened" });
+            return;
+        });
 });
 
 module.exports = router;
